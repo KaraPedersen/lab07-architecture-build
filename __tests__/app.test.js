9079252161c3dcf3172b04c4e3e71d29c473e5d7
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-06-17 20:26:48 -0700

    updated twilio and a bunch of other stuff

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,25 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Order from '../lib/models/Order.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  test('creates a new order in our database and sends a text message', async() => {
+    const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ 
+        item: 'food',
+        quantity: 100
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      item: 'food',
+      quantity: 100
+    });
   });
 });
